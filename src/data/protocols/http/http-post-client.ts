4b05aa5cc4a6f2604d81cd14aedff9a77eae7dd3
Ricardo Gobbo <rg.aguas@gmail.com>
2020-07-11 23:40:24 -0300

    feat: ensure RemoteAuthentication throw InvalidCredentialsError if HttpPostClient return 401

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,9 +1,12 @@
+import { HttpResponse } from '@/data/protocols/http/http-response'
+
 export type HttpPostParams = {
   url: string
   body?: object
+  response?: HttpResponse
   headers?: any
 }
 
 export interface HttpPostClient {
-  post(params: HttpPostParams): Promise<void>
+  post(params: HttpPostParams): Promise<HttpResponse>
 }
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,7 @@
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 
 export class RemoteAuthentication {
   constructor(
@@ -8,6 +10,16 @@ export class RemoteAuthentication {
   ) {}
 
   async auth(credentials?: AuthenticationParams): Promise<void> {
-    await this.httpPostClient.post({ url: this.url, body: credentials })
+    const response = await this.httpPostClient.post({
+      url: this.url,
+      body: credentials,
+    })
+
+    switch (response.statusCode) {
+      case HttpStatusCode.unauthorized:
+        throw new InvalidCredentialsError()
+      default:
+        return Promise.resolve()
+    }
   }
 }

b443b534d5a3543e438b3baa03763023de4326c7
Ricardo Gobbo <rg.aguas@gmail.com>
2020-07-12 01:08:58 -0300

    feat: ensure RemoteAuthentication returns an AccountModel when HttpPostClient Returns 200

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,12 +1,11 @@
 import { HttpResponse } from '@/data/protocols/http/http-response'
 
-export type HttpPostParams = {
+export type HttpPostParams<T> = {
   url: string
-  body?: object
-  response?: HttpResponse
+  body?: T
   headers?: any
 }
 
-export interface HttpPostClient {
-  post(params: HttpPostParams): Promise<HttpResponse>
+export interface HttpPostClient<T, R> {
+  post(params: HttpPostParams<T>): Promise<HttpResponse<R>>
 }
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,26 +1,27 @@
+import { AccountModel } from '@/domain/models/account-model'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
-    private readonly httpPostClient: HttpPostClient
+    private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  async auth(credentials?: AuthenticationParams): Promise<void> {
+  async auth(credentials?: AuthenticationParams): Promise<AccountModel> {
     const response = await this.httpPostClient.post({
       url: this.url,
       body: credentials,
     })
 
     switch (response.statusCode) {
+      case HttpStatusCode.ok:
+        return response.body
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
-      case HttpStatusCode.ok:
-        break
       default:
         throw new UnexpectedError()
     }
